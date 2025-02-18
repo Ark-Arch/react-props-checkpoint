9ba3d113ad6c309a88c445cb30d50c0a38a792ad
Ark-Arch <69996990+Ark-Arch@users.noreply.github.com>
2025-02-18 15:51:46 +0000

    compiled players' data

---
+++ checkpoint/src/assets/players.js
@@ -0,0 +1,35 @@
+
+const players = [
+    {   
+        name:"Messi", 
+        team:"FC Barcelona", 
+        nationality:"Argentinean",
+        jerseyNumber:10,
+        age: 37,
+        url:''
+    },
+    {
+        name:"Ronaldo",
+        team:"Real Madrid",
+        nationality:"Portuguese",
+        jerseyNumber:7,
+        age:40,
+        url:''
+    },
+    {
+        name:"Lamine Yamal",
+        team:"FC Barcelona",
+        nationality:"Spanish",
+        jerseyNumber:19,
+        age:17,
+        url:''
+    },
+    {
+        name:"Pau Cubarsi",
+        team:"FC Barcelona",
+        nationality:"Spanish",
+        jerseyNumber:2,
+        age:18,
+        url:''
+    }
+]
